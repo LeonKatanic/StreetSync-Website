6fa48ffae28418e54ce7cccfc64daa1b1f6ce94e
keon <101515179+LeonKatanic@users.noreply.github.com>
2024-06-07 18:22:17 +0200

    Add files via upload

--- HomePage.php
+++ HomePage.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['user_email'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -73,12 +74,34 @@ if (isset($_SESSION['user_email'])) {
                 <li><a href="#"><i class="bi bi-house"></i>Home</a></li>
                 <li><a href="#"><i class="bi bi-megaphone"></i>Announcments</a></li>
                 <li><a href="#"><i class="bi bi-calendar-event"></i>Events</a></li>
-                <li><a href="#"><i class="bi bi-shop-window"></i>Markteplace</a></li>
+                <li><a href="#"><i class="bi bi-shop-window"></i>Marketplace</a></li>
                 <li><a href="#"><i class="bi bi-pencil"></i>Reviews</a></li>
                 <li><a href="#"><i class="bi bi-hammer"></i>Jobs</a></li>
             </ul>
         </div>
     </div>
+
+    <!--Posts & comments-->
+    <div class="form-container">
+        <br>
+        <form method="POST" id="commentForm">
+            <div class="form-group">
+                <input type="text" name="name" id="name" class="form-control" placeholder="Enter Name" required />
+            </div>
+            <div class="form-group">
+                <textarea name="comment" id="comment" class="form-control" placeholder="Enter Comment" rows="5" required></textarea>
+            </div>
+            <span id="message"></span>
+            <br>
+            <div class="form-group">
+                <input type="hidden" name="commentId" id="commentId" value="0" />
+                <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Post Comment" />
+            </div>
+        </form>		
+        <br>
+        <div id="showComments"></div>   
+    </div>
+    <script src="scripts/comments.js"></script>
     <script>
         const subMenu = document.getElementById("subMenu");
 
